fea627b6efbb82e65188c5593038140b62d41516
Manuel Bovo <manuel.bovo@gmail.com>
2022-11-28 12:00:27 +0100

    Adding click and fixing things

--- src/nabot/main.py
+++ src/nabot/main.py
@@ -4,16 +4,82 @@
 import lnetatmo
 from influxdb import InfluxDBClient
 from rich import print
+import click
 import time
-import os
-
-CLIENT_ID = os.getenv("NABOT_CLIENT_ID")
-CLIENT_SECRET = os.getenv("NABOT_CLIENT_SECRET")
-USERNAME = os.getenv("NABOT_USERNAME")
-PASSWORD = os.getenv("NABOT_PASSWORD")
 
 
+@click.group(help="nabot - Netatmo to InfluxDB/Grafana bot")
 def main():
+    pass
+
+
+@main.command(help="Start the bot, it will scrapes NetAtmo APIs every X secs")
+@click.option(
+    "-i",
+    "--id",
+    "CLIENT_ID",
+    envvar="NABOT_CLIENT_ID",
+    required=True,
+    default=None,
+    help="Netatmo API ID",
+)
+@click.option(
+    "-s",
+    "--secret",
+    "CLIENT_SECRET",
+    envvar="NABOT_CLIENT_SECRET",
+    required=True,
+    default=None,
+    help="Netatmo API Secret",
+)
+@click.option(
+    "-u",
+    "--username",
+    "USERNAME",
+    envvar="NABOT_USERNAME",
+    required=True,
+    default=None,
+    help="Netatmo API username",
+)
+@click.option(
+    "-p",
+    "--password",
+    "PASSWORD",
+    envvar="NABOT_PASSWORD",
+    required=True,
+    default=None,
+    help="Netatmo API password",
+)
+@click.option(
+    "-h",
+    "--influx-host",
+    "INFLUXDB_HOST",
+    envvar="INFLUXDB_HOST",
+    required=False,
+    default="localhost",
+    help="Influx DB hostname",
+)
+@click.option(
+    "-D",
+    "--dry-run",
+    "DRY_RUN",
+    required=False,
+    default=False,
+    type=click.BOOL,
+    is_flag=True,
+    help="Dry run, do not write to InfluxDB",
+)
+@click.option(
+    "-t",
+    "--interval",
+    "INTERVAL",
+    required=False,
+    default=30,
+    help="Interval in seconds between each scrape",
+)
+def start(
+    CLIENT_ID, CLIENT_SECRET, USERNAME, PASSWORD, INFLUXDB_HOST, DRY_RUN, INTERVAL
+):
     authorization = lnetatmo.ClientAuth(
         clientId=CLIENT_ID,
         clientSecret=CLIENT_SECRET,
@@ -22,9 +88,10 @@ def main():
         scope="read_station",
     )
 
-    client = InfluxDBClient(host=os.getenv("NABOT_INFLUXDB_HOST"))
-    if {"name": "netatmo"} not in client.get_list_database():
-        client.create_database("netatmo")
+    if not DRY_RUN:
+        client = InfluxDBClient(INFLUXDB_HOST)
+        if {"name": "netatmo"} not in client.get_list_database():
+            client.create_database("netatmo")
 
     while True:
 
@@ -98,9 +165,12 @@ def main():
                             }
                         )
 
-            client.write_points(station_data, time_precision="s", database="netatmo")
-            client.write_points(module_data, time_precision="s", database="netatmo")
+            if not DRY_RUN:
+                client.write_points(
+                    station_data, time_precision="s", database="netatmo"
+                )
+                client.write_points(module_data, time_precision="s", database="netatmo")
 
-            print(station)
-        print("Sleep for 5s")
-        time.sleep(5)
+            print(module_data)
+        print(f"Sleep for {INTERVAL}s")
+        time.sleep(INTERVAL)
